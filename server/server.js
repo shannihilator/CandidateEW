9632343ba61f57c4ccdb5641f3e7410ad011929a
anonymous <anonymous>
2018-11-11 17:21:22 -0500

    add get endpoint

--- server/server.js
+++ server/server.js
@@ -1,29 +1,33 @@
 const express = require('express');
+const fetch = require('node-fetch');
 
-require('dotenv').config();
+require('dotenv').config({path: '../.env'});
 
 const { PORT } = require('../config');
 
 const app = express();
 app.use(express.static('public'));
-console.log("hello world!");
+
 
 app.get('/people', (req, res) => {
 
   const baseUrl = 'https://api.salesloft.com/v2/people.json'
-  const { KEY } = require('dotenv').config();
+  const apiKey = process.env.API_KEY
 
   fetch(baseUrl, {
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${KEY}`
-
+      'Authorization': `Bearer ${apiKey}`
     }
   })
-
-})
-
-
+  .then(res => res.json())
+  .then((data) => {
+    res.send(data);
+  })
+  .catch((err) => {
+    res.status(500).json(err);
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
